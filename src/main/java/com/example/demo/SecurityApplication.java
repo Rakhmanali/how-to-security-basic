a7806703945880aaca7a7f2648629799474932cc
rakhmanali <rkabekov@gmail.com>
2021-05-28 14:49:36 +0500

    the link of the origin code is added

--- src/main/java/com/example/demo/SecurityApplication.java
+++ src/main/java/com/example/demo/SecurityApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// https://www.youtube.com/watch?v=her_7pa0vrg&t=15131s&ab_channel=Amigoscode
 @SpringBootApplication
 public class SecurityApplication {
 
